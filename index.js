358bcb6e5407b17ba06c09a435ca31ac12ad1b69
Alexis <lxs.bnds@gmail.com>
2020-01-17 16:08:47 -0800

    add department and add employee added

--- index.js
+++ index.js
@@ -23,7 +23,105 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    pickTask();
 ;});
 
-//do not need a listener because we are not interacting with the browser
+function pickTask(){
+    inquirer.prompt([
+        {name: "task",
+        message: "Select a task",
+        type: "list",
+        choices: ["Add department", "Add employee", "Add role", "Update department", "Update employee", "Update role", "View department", "View employee", "View role","Quit"]
+    }]).then(function(res){
+      if(res.task === "Add department"){
+        addDepartment();
+      }
+      else if (res.task === "Add employee"){
+        addEmployee();
+      }
+    //   else if(res === "Add role"){
 
+    //   }
+    //   else if(res === "Update department"){
+
+    //   }
+    //   else if(res === "Update employee"){
+
+    //   }
+    //   else if(res === "Update role"){
+
+    //   }
+    //    else if(res === "View department"){
+
+    //   }
+    //   else if(res === "View employee"){
+
+    //   }
+    //   else if(res === "View role"){
+
+    //   }
+    })
+}
+
+function addDepartment(){
+    inquirer.prompt([
+        {name: "name",
+        message: "What is the new department's name?",
+        type: "input"
+        }]).then(function(res){
+            connection.query("INSERT INTO department (name) VALUES (?)", [res.name], function(err, result){
+                if(err) throw err;
+                console.log(result);
+                console.log("The department has been added");
+                pickTask();
+            })
+        })
+}
+function addEmployee(){
+    inquirer.prompt([
+        {name: "firstname",
+        message: "What is the new employee's first name?",
+        type: "input"
+        },
+        {name: "lastname",
+        message: "What is the new employee's last name?",
+        type: "input"
+        },
+        {name: "role",
+        message: "What is the new employee's role id?",
+        type: "number"
+        },
+        {name: "manager",
+        message: "What is the new employee's manager id?",
+        type: "number"
+        }
+    ]).then(function(res){
+            connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)", [res.firstname,res.lastname,res.role, res.manager], function(err, result){
+                if(err) throw err;
+                console.log(result)
+                console.log("The employee has been added");
+                pickTask();
+            
+})
+})}
+// function addRole(){
+
+// }
+// function updateDepartment(){
+
+// }
+// function updateEmployee(){
+
+// }
+// function updateRole(){
+
+// }
+// function viewDepartment(){
+
+// }
+// function viewEmployee(){
+
+// }
+// function viewRole(){
+
+// }
